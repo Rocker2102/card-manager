f560eee19cd08d7e426d58c08b831edcdbd0d94e
Ankush Yadav <ankushyadav9302@gmail.com>
2023-03-06 23:46:52 +0530

    Migrate to vite from cra

---
+++ ui/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), viteTsconfigPaths()],
+  build: {
+    outDir: "build"
+  },
+  server: {
+    host: true,
+    open: true,
+    port: 3000
+  }
+});
