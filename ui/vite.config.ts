8d444fde29befc7acc2abb29c1be52c117469437
Ankush Yadav <ankushyadav9302@gmail.com>
2023-03-07 22:03:30 +0530

    Setup pwa using vite

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   build: {
     outDir: "build"
   },
