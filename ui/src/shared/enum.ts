ed805351deb4b6f155855deaf5905bff3e7e32bd
Ankush Yadav <ankushyadav9302@gmail.com>
2023-03-12 21:48:35 +0530

    Add logic to restore theme from localstorage

--- ui/src/shared/enum.ts
+++ ui/src/shared/enum.ts
@@ -1,2 +1,7 @@
 export const DB_NAME = "pwa_card_manager";
 export const DB_VERSION = 1;
+
+export const LS_KEY_PREFIX = "pwa_card_manager_";
+export const LS_KEYS = {
+  themeMode: `${LS_KEY_PREFIX}themeMode`
+};
--- ui/src/shared/theme.ts
+++ ui/src/shared/theme.ts
@@ -1,10 +1,24 @@
 import { createTheme, ThemeOptions } from "@mui/material/styles";
+import { LS_KEYS } from "shared/enum";
+import { readFromStorage } from "shared/utils";
 
-// Create a theme instance. TODO: Move to a separate file, use from local storage
-const THEME: ThemeOptions = {
+type ThemeMode = "light" | "dark";
+
+const themeMode: ThemeMode =
+  readFromStorage<ThemeMode, ThemeMode>({
+    key: LS_KEYS.themeMode,
+    fallbackValue: "dark"
+  }) ?? "dark";
+
+// TODO: Move to a separate file, use from local storage
+const theme: ThemeOptions = {
   palette: {
-    mode: "dark"
+    mode: themeMode,
+    background: {
+      default: themeMode === "dark" ? "#202122" : "#f5f5f5",
+      paper: themeMode === "dark" ? "#2c2c2e" : "#fff"
+    }
   }
 };
 
-export default createTheme(THEME);
+export default createTheme(theme);
