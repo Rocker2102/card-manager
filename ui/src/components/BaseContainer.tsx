1b9fe32f380c381d6e7b75350b9bee30c1db0f18
Ankush Yadav <ankushyadav9302@gmail.com>
2023-03-13 01:58:37 +0530

    Update <BaseContainer /> component

--- ui/src/components/BaseContainer.tsx
+++ ui/src/components/BaseContainer.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Box, Divider, Typography } from "@mui/material";
+import { Box, Divider as MuiDivider, Typography } from "@mui/material";
+
+import type { Theme } from "@mui/system";
 
 type BaseContainerProps = {
   children?: React.ReactNode;
   pageTitle?: string;
 };
 
+// FIXME: Remove typescript error
 export default function BaseContainer({ children, pageTitle }: BaseContainerProps) {
   return (
     <StyledBox mt={2}>
@@ -28,3 +31,7 @@ export default function BaseContainer({ children, pageTitle }: BaseContainerProp
 const StyledBox = styled(Box)`
   text-align: center;
 `;
+
+const Divider = styled(MuiDivider)(({ theme }: { theme: Theme }) => ({
+  marginBottom: theme.spacing(2)
+}));
