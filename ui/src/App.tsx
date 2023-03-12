4b0bac09106cc253916a3d1c0b14e8bf07cfa35c
Ankush Yadav <ankushyadav9302@gmail.com>
2023-03-12 23:24:49 +0530

    Fix issue with applying of theme

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "@mui/system";
 import { RouterProvider } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
 
 import APP_THEME from "shared/theme";
 import APP_ROUTER from "shared/router";
@@ -11,6 +12,8 @@ console.log("APP_THEME", APP_THEME);
 export default function App() {
   return (
     <ThemeProvider theme={APP_THEME}>
+      <CssBaseline />
+
       <AppProvider>
         <RouterProvider router={APP_ROUTER} />
       </AppProvider>
