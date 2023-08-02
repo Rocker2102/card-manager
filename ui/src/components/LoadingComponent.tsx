b248cb8e7011c224f4d4a88a57add50d314ee130
Ankush Yadav <ankushyadav9302@gmail.com>
2023-08-02 22:11:28 +0530

    add <LoadingComponent />

---
+++ ui/src/components/LoadingComponent.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+
+interface LoadingComponentProps {
+  isLoading: boolean;
+  component: React.ReactElement;
+  size?: string;
+  color?: "inherit" | "primary" | "secondary" | "success" | "error" | "info" | "warning";
+}
+
+export default function LoadingComponent({
+  isLoading,
+  size = "2em",
+  color = "warning",
+  component
+}: LoadingComponentProps) {
+  return isLoading ? <CircularProgress size={size} color={color} /> : <> {component} </>;
+}
