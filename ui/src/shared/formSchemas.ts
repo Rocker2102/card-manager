8ee8132d4f1f51d607d92ff4adced1d9246855a3
Ankush Yadav <ankushyadav9302@gmail.com>
2023-08-03 21:54:22 +0530

    trim whitespaces before handling form data

--- ui/src/shared/formSchemas.ts
+++ ui/src/shared/formSchemas.ts
@@ -4,7 +4,7 @@ const getMessagesObj = (fieldName = "Field") => {
   return {
     "any.required": `${fieldName} is required`,
     "string.base": `${fieldName} must be a string`,
-    "string.empty": `${fieldName} cannot be empty`,
+    "string.empty": `${fieldName} cannot be blank`,
     "string.pattern.base": `${fieldName} is invalid`,
     "string.min": `${fieldName} is too short`,
     "string.max": `${fieldName} is too long`
@@ -19,19 +19,27 @@ export const addBankAccountSchema = Joi.object({
     .required()
     .messages(getMessagesObj("Account number")),
   accountHoldersName: Joi.string()
+    .trim()
     .required()
     .min(3)
     .messages(getMessagesObj("Account holder's name")),
   ifscCode: Joi.string()
+    .trim()
     .empty("")
     .allow("")
     .pattern(/[A-Z|a-z]{4}[0][a-zA-Z0-9]{6}$/)
     .messages(getMessagesObj("IFSC code")),
   mmid: Joi.string()
+    .trim()
     .empty("")
     .allow("")
     .pattern(/^[0-9]{7}$/)
     .messages(getMessagesObj("MMID")),
-  nomineesName: Joi.string().empty("").allow("").min(3).messages(getMessagesObj("Nominee's name")),
+  nomineesName: Joi.string()
+    .trim()
+    .empty("")
+    .allow("")
+    .min(3)
+    .messages(getMessagesObj("Nominee's name")),
   syncWithCloud: Joi.boolean().required().messages(getMessagesObj("Sync with cloud"))
 });
