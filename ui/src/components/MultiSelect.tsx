f46bb8a44227a40c0d43315a2031894adbc89418
Ankush Yadav <ankushyadav9302@gmail.com>
2023-08-05 16:13:06 +0530

    update <MultiSelect />

--- ui/src/components/MultiSelect.tsx
+++ ui/src/components/MultiSelect.tsx
@@ -2,13 +2,15 @@ import * as React from "react";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
+import FormControl, { FormControlProps } from "@mui/material/FormControl";
+import FormHelperText, { FormHelperTextProps } from "@mui/material/FormHelperText";
 import ListItemText from "@mui/material/ListItemText";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Select, { SelectChangeEvent, SelectProps } from "@mui/material/Select";
 import Checkbox from "@mui/material/Checkbox";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
+
 const MenuProps = {
   PaperProps: {
     style: {
@@ -19,54 +21,66 @@ const MenuProps = {
 };
 
 type OptionType = {
-  id: string;
-  label: string;
+  [id: string]: string;
 };
 
 interface MultiSelectProps {
-  options: OptionType[];
+  options: OptionType;
   labelId: string;
   name?: string;
   label: string;
+  selected: string[];
+  setSelected: (arr: string[]) => void;
+  helperText?: string;
+  helperTextProps?: FormHelperTextProps;
+  formControlProps?: FormControlProps;
+  selectProps?: SelectProps;
 }
 
 export default function MultiSelect({
   options,
   label,
   labelId,
-  name = undefined
+  name = undefined,
+  helperText = undefined,
+  selected,
+  setSelected,
+  helperTextProps = {},
+  formControlProps = {},
+  selectProps = {}
 }: MultiSelectProps) {
-  const [selectedOptions, setSelectedOptions] = React.useState<string[]>([]);
-
-  const handleChange = (event: SelectChangeEvent<typeof selectedOptions>) => {
+  const handleChange = (event: SelectChangeEvent<typeof selected>) => {
     const {
       target: { value }
     } = event;
-    setSelectedOptions(typeof value === "string" ? value.split(",") : value);
+    setSelected(typeof value === "string" ? value.split(",") : value);
   };
 
   return (
-    <div>
-      <FormControl>
-        <InputLabel id={labelId}>{label}</InputLabel>
-        <Select
-          labelId={labelId}
-          multiple
-          name={name}
-          value={selectedOptions}
-          onChange={handleChange}
-          input={<OutlinedInput label="Tag" />}
-          renderValue={selected => selected.join(", ")}
-          MenuProps={MenuProps}
-        >
-          {options.map(({ id, label }: OptionType) => (
-            <MenuItem key={id} value={id}>
-              <Checkbox checked={selectedOptions.indexOf(id) > -1} />
-              <ListItemText primary={label} />
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
+    <FormControl {...formControlProps}>
+      <InputLabel id={labelId}>{label}</InputLabel>
+      <Select
+        {...selectProps}
+        labelId={labelId}
+        label={label}
+        multiple
+        name={name}
+        value={selected}
+        onChange={handleChange}
+        input={<OutlinedInput label="Tag" />}
+        renderValue={selected => selected.map(id => options[id]).join(", ")}
+        MenuProps={MenuProps}
+        defaultValue={[]}
+      >
+        {Object.keys(options).map((id: string) => (
+          <MenuItem key={id} value={id}>
+            <Checkbox checked={selected.indexOf(id) > -1} />
+            <ListItemText primary={options[id]} />
+          </MenuItem>
+        ))}
+      </Select>
+
+      <FormHelperText {...helperTextProps}>{helperText}</FormHelperText>
+    </FormControl>
   );
 }
