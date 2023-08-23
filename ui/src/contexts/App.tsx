4c7384b9b5c6834fc063c85d52b4f8c9d70b15fe
Ankush Yadav <ankushyadav9302@gmail.com>
2023-08-23 12:31:03 +0530

    add <LoadingOverlay /> to app context for global access

--- ui/src/contexts/App.tsx
+++ ui/src/contexts/App.tsx
@@ -1,8 +1,9 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useCallback } from "react";
 import useToast from "hooks/useToast";
 
 import APP_THEME from "shared/theme";
 import { getAppSettings } from "shared/helpers";
+import LoadingOverlay from "components/LoadingOverlay";
 
 import type { AlertProps } from "@mui/material";
 
@@ -10,7 +11,9 @@ export const AppContext = createContext({
   /* eslint-disable */
   appTheme: [APP_THEME, (theme: typeof APP_THEME) => {}] as const,
   appSettings: [getAppSettings(), (settings: AppSettings) => {}] as const,
-  showToast: (message: string, alertType?: AlertProps["severity"], autoCloseAfter?: number) => {}
+  showToast: (message: string, alertType?: AlertProps["severity"], autoCloseAfter?: number) => {},
+  showLoadingOverlay: () => {},
+  hideLoadingOverlay: () => {}
   /* eslint-enable */
 });
 
@@ -21,17 +24,24 @@ export type AppProviderProps = {
 export function AppProvider({ children }: AppProviderProps) {
   const [theme, setTheme] = useState<typeof APP_THEME>(APP_THEME);
   const [settings, setSettings] = useState<AppSettings>(getAppSettings());
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const { showToast, ToastWithCloseButton } = useToast();
+
+  const showLoadingOverlay = useCallback(() => setIsLoading(true), []);
+  const hideLoadingOverlay = useCallback(() => setIsLoading(false), []);
 
   return (
     <AppContext.Provider
       value={{
         appTheme: [theme, setTheme],
         appSettings: [settings, setSettings],
+        showLoadingOverlay,
+        hideLoadingOverlay,
         showToast
       }}
     >
       <ToastWithCloseButton />
+      <LoadingOverlay loading={isLoading} />
       {children}
     </AppContext.Provider>
   );
