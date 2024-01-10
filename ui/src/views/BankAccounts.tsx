887182b316f024f4f97efd89f6e74af1e7a01b46
Ankush Yadav <ankushyadav9302@gmail.com>
2024-01-10 21:16:33 +0530

    use new hooks in actual component

--- ui/src/views/BankAccounts.tsx
+++ ui/src/views/BankAccounts.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Stack } from "@mui/material";
 import { Add as AddIcon, CloudSync as CloudSyncIcon } from "@mui/icons-material";
 
 import { v4 } from "uuid";
 import { addAccount } from "database/bankAccountService";
+import useDbReady from "hooks/query/idb/useDbReady";
+import useGetBankAccounts from "hooks/query/idb/useGetBankAccounts";
 import ButtonFit from "components/ButtonFit";
 import AddBankAccountDialogDialog from "components/AddBankAccountDialog";
 import BaseContainer from "components/BaseContainer";
@@ -12,6 +14,26 @@ import type { AddBankAccountInput } from "typings/forms";
 
 export default function BankAccountsView() {
   const [isOpen, setIsOpen] = useState(false);
+  const {
+    data: bankAccounts,
+    isLoading: bankAccountsLoading,
+    isError: bankAccountsError
+  } = useGetBankAccounts({ enabled: useDbReady() });
+
+  useEffect(() => {
+    if (bankAccountsError) {
+      console.error(bankAccountsError);
+    }
+
+    if (bankAccountsLoading) {
+      console.log("Loading bank accounts...");
+    }
+
+    if (bankAccounts) {
+      console.log("Bank accounts loaded successfully");
+      console.log(bankAccounts);
+    }
+  }, [bankAccounts, bankAccountsLoading, bankAccountsError]);
 
   const handleNewBankAccountCreation = async (data: AddBankAccountInput) => {
     const currDate = new Date();
