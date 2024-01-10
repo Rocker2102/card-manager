d99c385eacfce01175ef55cd95a9ffbdb45894d7
Ankush Yadav <ankushyadav9302@gmail.com>
2024-01-10 20:18:24 +0530

    initialize query client & global provider

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,21 @@
 import React, { useContext } from "react";
 import { ThemeProvider } from "@mui/system";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import CssBaseline from "@mui/material/CssBaseline";
 
 import APP_ROUTER from "shared/router";
 import { AppContext } from "contexts/App";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      retry: false,
+      refetchOnWindowFocus: false
+    }
+  }
+});
 
 export default function App() {
   const { appTheme } = useContext(AppContext);
@@ -14,7 +25,9 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
 
-      <RouterProvider router={APP_ROUTER} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={APP_ROUTER} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
