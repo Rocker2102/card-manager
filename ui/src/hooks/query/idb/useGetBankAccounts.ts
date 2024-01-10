2236ed2c6c7fbd69dd236f4c4186debe5d75e44d
Ankush Yadav <ankushyadav9302@gmail.com>
2024-01-10 21:16:09 +0530

    add support for props to useGetBankAccounts() hook

--- ui/src/hooks/query/idb/useGetBankAccounts.ts
+++ ui/src/hooks/query/idb/useGetBankAccounts.ts
@@ -1,11 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import appDb from "database/app";
 
-export default function useGetBankAccounts() {
+import type { UseQueryOptions } from "@tanstack/react-query";
+
+export default function useGetBankAccounts(props?: Omit<UseQueryOptions, "queryKey" | "queryFn">) {
   return useQuery({
     queryKey: ["bank-accounts"],
     queryFn: () => {
       return appDb.bankAccounts.toArray();
-    }
+    },
+    enabled: props?.enabled
   });
 }
