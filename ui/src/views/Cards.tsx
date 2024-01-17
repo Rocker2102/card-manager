8cf3aa809ec95a089cf4488c5738880b61c2ca79
Ankush Yadav <ankushyadav9302@gmail.com>
2024-01-17 18:10:17 +0530

    update base cards view

--- ui/src/views/Cards.tsx
+++ ui/src/views/Cards.tsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useState, useRef, useContext } from "react";
+import Stack from "@mui/material/Stack";
+import {
+  Add as AddIcon,
+  CloudSync as CloudSyncIcon,
+  RefreshRounded as RefreshIcon
+} from "@mui/icons-material";
 
+import { v4 } from "uuid";
+import { AppContext } from "contexts/App";
+// import { addCard, deleteCard, updateCard } from "database/cardService";
+import useDbReady from "hooks/query/idb/useDbReady";
+import useGetCards from "hooks/query/idb/useGetCards";
 import BaseContainer from "components/BaseContainer";
+import DeleteConfirmationDialog from "components/DeleteConfirmationDialog";
+import ButtonFit from "components/ButtonFit";
+import DisplayLoadingAlerts from "components/DisplayLoadingAlerts";
+
+import type { Card } from "database/types";
 
 export default function CardsView() {
-  return <BaseContainer pageTitle="Saved cards"></BaseContainer>;
+  const { showToast } = useContext(AppContext);
+  const dbReady = useDbReady();
+  const [deleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
+  const {
+    data: cards,
+    isLoading: cardsLoading,
+    isFetching: cardsFetching,
+    isError: cardsError,
+    refetch: refetchCards
+  } = useGetCards({ enabled: dbReady });
+  const selectedCardRef = useRef<Card | null>(null);
+
+  return (
+    <BaseContainer pageTitle="Saved cards">
+      <DeleteConfirmationDialog
+        open={deleteConfirmationOpen}
+        infoText="This action cannot be undone."
+        onClose={() => setDeleteConfirmationOpen(false)}
+        onConfirm={() => {
+          console.log("Delete confirmation dialog");
+        }}
+      />
+
+      <Stack spacing={1} direction="row" mb={3} justifyContent="space-between">
+        <ButtonFit
+          buttonProps={{
+            variant: "outlined",
+            startIcon: <AddIcon />,
+            onClick: () => true
+          }}
+        >
+          New
+        </ButtonFit>
+
+        <Stack spacing={1} direction="row">
+          <ButtonFit
+            buttonProps={{
+              variant: "outlined",
+              startIcon: <RefreshIcon />,
+              color: "secondary",
+              onClick: () => refetchCards(),
+              disabled: cardsFetching
+            }}
+          >
+            Reload
+          </ButtonFit>
+
+          <ButtonFit
+            buttonProps={{ variant: "outlined", startIcon: <CloudSyncIcon />, disabled: true }}
+          >
+            Sync
+          </ButtonFit>
+        </Stack>
+      </Stack>
+
+      <DisplayLoadingAlerts
+        error={cardsError}
+        loading={cardsLoading}
+        fetching={cardsFetching}
+        dataLength={cards?.length}
+        messages={{
+          error: "There was a problem loading your saved cards.",
+          noData: "No cards to display. Add a new card to view it here."
+        }}
+      />
+    </BaseContainer>
+  );
 }
